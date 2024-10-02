741968d865c403d9daee121495e74c216936a38d
Narae Gwon <rnjs021031@gmail.com>
2024-10-02 17:45:20 +0900

    20241002 sharpening/embossing, edge 검출

--- ch3-2/3-10c_median.py
+++ ch3-2/3-10c_median.py
@@ -8,7 +8,7 @@ cv2.imshow('Smooth - Median',median)
 
 #1 다양한 크기의 필터
 median1=np.hstack((gray,cv2.medianBlur(gray, 3),cv2.medianBlur(gray, 7),cv2.medianBlur(gray, 11)))
-#cv2.imshow('Smooth - Median1',median1)
+cv2.imshow('Smooth - Median1',median1)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
